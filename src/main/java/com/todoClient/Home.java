8a3d615185f2d2ffae0c85ac8e73c36375103107
KentoTokura <tkento1985@gmail.com>
2023-07-19 07:58:46 +0900

    making_home_screen

--- src/main/java/com/todoClient/Home.java
+++ src/main/java/com/todoClient/Home.java
@@ -2,7 +2,6 @@ package com.todoClient;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -15,9 +14,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 
 public class Home extends Window {
     JFrame frame;
@@ -36,13 +32,9 @@ public class Home extends Window {
 
     class ButtonAction implements ActionListener {
         private String label_txt = "";
-        private JTextField emailField;
-        private JPasswordField passwordField;
 
-        ButtonAction(JTextField emailField, JPasswordField passwordField, String txt) {
+        ButtonAction(String txt) {
             this.label_txt = txt;
-            this.passwordField = passwordField;
-            this.emailField = emailField;
         }
 
         public void actionPerformed(ActionEvent e) {
@@ -51,28 +43,6 @@ public class Home extends Window {
                 moveLogin();
             } else if (this.label_txt == "submit") {
                 System.out.println("submit");
-                // String email = this.emailField.getText();
-                // String password = String.valueOf(this.passwordField.getPassword());
-                // int return_flag = 0;
-                // if (email.equals("")) {
-                // emailField.setBackground(java.awt.Color.pink);
-                // return_flag = 1;
-                // System.out.println("email is empty");
-                // } else {
-                // emailField.setBackground(java.awt.Color.white);
-                // }
-                // if (password.equals("")) {
-                // passwordField.setBackground(java.awt.Color.pink);
-                // return_flag = 1;
-                // System.out.println("password is empty");
-                // } else {
-                // passwordField.setBackground(java.awt.Color.white);
-                // }
-                // if (return_flag == 1) {
-                // return;
-                // }
-                // User user = new User("", email, password, "");
-                // moveLogin();
             } else {
                 System.out.println("undefined");
             }
@@ -86,71 +56,67 @@ public class Home extends Window {
         JFrame new_frame = new JFrame();
         JFrame left_frame = new JFrame();
         left_frame.setSize(main_width / 3, main_height);
+        JFrame left_upper_frame = new JFrame();
+        left_upper_frame.setSize(main_width / 3, main_height / 10 * 9);
         JFrame left_bottom_frame = new JFrame();
         left_bottom_frame.setSize(main_width / 3, main_height / 10);
         JFrame right_frame = new JFrame();
         right_frame.setSize(main_width / 3 * 2, main_height);
 
-        JPanel inputPanel = new JPanel(new GridBagLayout());
+        // 仮想タスクデータ
+        Task t1 = new Task(0, "0", "title1", "body", "2020-01-01");
+        Task t2 = new Task(1, "1", "title2", "body", "2020-01-01");
+        Task t3 = new Task(2, "2", "title3", "body", "2020-01-01");
+        Task[] tasks = new Task[] { t1, t2, t3 };
+        //
+
+        JPanel tasks_panel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(2, 2, 10, 0); // 入力ボックスの周囲の余白を設定
-        JLabel emailLabel = new JLabel("Email Address:");
-        JTextField emailField = new JTextField(20);
-        JLabel passwordLabel = new JLabel("Password:");
-        JPasswordField passwordField = new JPasswordField(20);
+        gbc.insets = new Insets(2, 2, 2, 2); // ボックスの周囲の余白を設定
+
+        for (int x = 0; x < 10; x++) {
+            for (int i = 0; i < tasks.length; i++) {
+                gbc.gridx = 0;
+                gbc.gridy = i;
+                JLabel task_label = new JLabel(tasks[i].title);
+                tasks_panel.add(task_label, gbc);
+                gbc.gridx = 1;
+            }
+        }
+
         // 入力ボックスの高さを調整
-        Font font = new Font("Arial", Font.PLAIN, 16); // フォントサイズを16に設定
-        emailField.setFont(font);
-        passwordField.setFont(font);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        inputPanel.add(emailLabel, gbc);
-        gbc.gridx = 1;
-        inputPanel.add(emailField, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        inputPanel.add(passwordLabel, gbc);
-        gbc.gridx = 1;
-        inputPanel.add(passwordField, gbc);
-        inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        inputPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        JFrame scroll_frame = new JFrame();
-        JScrollPane scrollPane = new JScrollPane(inputPanel);
-        scroll_frame.add(scrollPane);
+        tasks_panel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        tasks_panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // JFrame scroll_frame = new JFrame();
+        // JScrollPane scrollPane = new JScrollPane(tasks_panel);
+        // scroll_frame.add(scrollPane);
+        // left_upper_frame.getContentPane().add(scroll_frame, BorderLayout.CENTER);
+        left_upper_frame.getContentPane().add(tasks_panel, BorderLayout.CENTER);
 
-        JPanel pane_submit = new JPanel();
-        pane_submit.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        pane_submit.setLayout(new GridLayout(1, 1));
-        JButton submit = new JButton("SIGN IN");
-        ButtonAction submit_listener = new ButtonAction(emailField, passwordField, "submit");
-        submit.addActionListener(submit_listener);
-        pane_submit.add(submit);
+        JPanel logout_panel = new JPanel();
+        JButton logout = new JButton("Log out");
+        ButtonAction logout_listener = new ButtonAction("logout");
+        logout.addActionListener(logout_listener);
+        logout.setHorizontalAlignment(JButton.CENTER);
+        // logout.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+        logout_panel.setLayout(new GridLayout(1, 1));
+        logout_panel.add(logout);
+        left_bottom_frame.getContentPane().add(logout_panel, BorderLayout.CENTER);
 
-        JPanel pane0 = new JPanel();
-        JLabel label = new JLabel("SIGN IN");
+        JLabel label = new JLabel("Hello world!");
         label.setHorizontalAlignment(JLabel.CENTER);
-        pane0.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
-        pane0.setLayout(new GridLayout(1, 1));
-        pane0.add(label);
+        right_frame.setLayout(new GridLayout(1, 1));
+        right_frame.add(label);
 
-        JPanel pane1 = new JPanel();
-        JButton back = new JButton("< BACK");
-        ButtonAction back_listener = new ButtonAction(emailField, passwordField, "back");
-        back.addActionListener(back_listener);
-        back.setHorizontalAlignment(JButton.CENTER);
-        pane1.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
-        pane1.setLayout(new GridLayout(1, 1));
-        pane1.add(back);
+        // JFrame top_frame = new JFrame();
+        // top_frame.setSize(frame.getWidth(), main_height / 10);
+        // top_frame.getContentPane().add(pane0, BorderLayout.CENTER);
+        // top_frame.getContentPane().add(pane1, BorderLayout.WEST);
 
-        JFrame top_frame = new JFrame("");
-        top_frame.setSize(frame.getWidth(), main_height / 10);
-        top_frame.getContentPane().add(pane0, BorderLayout.CENTER);
-        top_frame.getContentPane().add(pane1, BorderLayout.WEST);
-
-        new_frame.getContentPane().add(top_frame.getContentPane(), BorderLayout.NORTH);
-        new_frame.getContentPane().add(scroll_frame.getContentPane(), BorderLayout.CENTER);
-        new_frame.getContentPane().add(pane_submit, BorderLayout.SOUTH);
-
+        left_frame.getContentPane().add(left_upper_frame.getContentPane(), BorderLayout.NORTH);
+        left_frame.getContentPane().add(left_bottom_frame.getContentPane(), BorderLayout.SOUTH);
+        new_frame.getContentPane().add(left_frame.getContentPane(), BorderLayout.WEST);
+        new_frame.getContentPane().add(right_frame.getContentPane(), BorderLayout.EAST);
         return new_frame.getContentPane();
     }
 }
--- src/main/java/com/todoClient/Task.java
+++ src/main/java/com/todoClient/Task.java
@@ -7,4 +7,11 @@ public class Task extends History {
     String body;
     String due_date;
 
+    Task(long id, String task_set_id, String title, String body, String due_date) {
+        this.id = id;
+        this.task_set_id = task_set_id;
+        this.title = title;
+        this.body = body;
+        this.due_date = due_date;
+    }
 }
