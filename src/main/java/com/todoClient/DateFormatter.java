c0e0ccc34b703fc31357418dde9a686e034e834c
daichi1207 <daichiyashima@gmail.com>
2023-07-21 20:43:58 +0900

    feat: added usercsv, dateTransformer, putMethod, getMethod

--- src/main/java/com/todoClient/DateFormatter.java
+++ src/main/java/com/todoClient/DateFormatter.java
@@ -3,14 +3,29 @@ package com.todoClient;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateFormatter {
+    //public static Date format(String originalDateStr) {
+    //    System.out.println("変換前の日時：" + originalDateStr);
+    //    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+    //    try {
+    //        Date date = sdf.parse(originalDateStr);
+    //        System.out.println("変換された日時：" + date);
+    //        return date;
+    //    } catch (ParseException e) {
+    //        e.printStackTrace();
+    //        return null;
+    //    }
+    //}
+
     public static Date format(String originalDateStr) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        SimpleDateFormat originalFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        SimpleDateFormat targetFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         try {
-            Date date = sdf.parse(originalDateStr);
-            System.out.println("変換された日時：" + date);
-            return date;
+            Date date = originalFormat.parse(originalDateStr);
+            String formattedDateString = targetFormat.format(date);
+            return targetFormat.parse(formattedDateString);
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
--- src/main/java/com/todoClient/TaskDTO.java
+++ src/main/java/com/todoClient/TaskDTO.java
@@ -7,17 +7,21 @@ public class TaskDTO {
     String body;
     int priority;
     Date due_date;
-    boolean is_completed;
-    boolean is_archived_on_completion;
+    boolean _completed;
+    boolean _archived_on_completion;
+    Date created_at;
+    Date updated_at;
     long[] shared_users;
 
-    TaskDTO(String title, String body, int priority, Date due_date,  long[] shared_users) {
+    TaskDTO(String title, String body, int priority, Date due_date, boolean is_completed, boolean is_archived_on_completion,  Date created_at,  Date updated_at,  long[] shared_users) {
         this.title = title;
         this.body = body;
         this.priority = priority;
         this.due_date = due_date;
-        this.is_completed = false;
-        this.is_archived_on_completion = false;
+        this._completed = is_completed;
+        this._archived_on_completion = is_archived_on_completion;
+        this.created_at = new Date();
+        this.updated_at = new Date();
         this.shared_users = shared_users;
     }
 
@@ -31,13 +35,4 @@ public class TaskDTO {
         return task;
     }
 
-    public static Task put_task (String access_token, Task taskDTO, long id) {
-        String endpoint = "/tasks/" + id;
-        OpenAPI_client c = new OpenAPI_client();
-        c.set_access_token(access_token);
-        String response = c.put(endpoint, Json.stringify(taskDTO));
-        System.out.println(response);
-        Task task = Json.parse(Task.class, response);
-        return task;
-    }
 }
